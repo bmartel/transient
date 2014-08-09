95834e7df579a5e9ba8177ac9008f3779e4e6094
bmartel <brandonmartel@gmail.com>
2014-08-09 11:44:24 -0500

    more encoding of signatures

--- src/Bmartel/Transient/TransientPropertyTrait.php
+++ src/Bmartel/Transient/TransientPropertyTrait.php
@@ -18,7 +18,12 @@ trait TransientPropertyTrait
     {
         $key = Config::get('app.key'); // Sign the signature with the app key to know it came from this app.
 
-        $property = json_encode(['id' => $this->getKey(), 'model' => static::class, 'property' => $property]);
+        $property = json_encode([
+            'id' => $this->getKey(),
+            'model' => static::class,
+            'property' => $property,
+            'created' => $this->created_at->timestamp
+        ]);
 
         return urlencode(base64_encode(hash_hmac('sha256', $property, $key, true)));
     }
